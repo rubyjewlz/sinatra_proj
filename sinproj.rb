c447c5274f14b422ab24b86d37a9bb25b6395117
Reuben <reuben.telushkin@gmail.com>
2015-06-26 11:38:57 -0400

    Added a Navigation Bar, created layout.erb so that the nav bar is constant throughout the site

--- sinproj.rb
+++ sinproj.rb
@@ -1,11 +1,34 @@
 require 'sinatra'
 
+require 'mandrill'
+
+
+post '/contact' do
+	@user_name = params[:username]
+
+	@confirmation_message = "Thanks for logging in"
+end
+
 get '/' do 
 	erb :sin_proj1
 end
 
-get '/About'
+
+get '/about' do
 
 erb :sin_proj2
 
 end
+
+get '/gallery' do
+
+erb :sin_proj3
+
+end
+
+get '/contact' do
+
+erb :sin_proj4
+
+end
+
